f218c002867a594dba6139cfb9e237cc0611487e
Paula Rojas Frias <rojasfriaspaula@gmail.com>
2023-09-21 14:42:34 -0300

    entrega final js terminada

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 class Requests {
-  constructor(nombre, apellido , email, numeroTelefono, tattooDescription, placement, approximateSize, dates) {
+  constructor(nombre, apellido, email, numeroTelefono, tattooDescription, placement, approximateSize, dates) {
     this.nombre = nombre;
-    this.apellido  = apellido ;
+    this.apellido = apellido;
     this.email = email;
-    this.numeroTelefono =numeroTelefono ;
+    this.numeroTelefono = numeroTelefono;
     this.tattooDescription = tattooDescription;
     this.placement = placement;
-    this. approximateSize =  approximateSize;
+    this.approximateSize = approximateSize;
     this.dates = dates;
   }
 }
@@ -23,20 +23,43 @@ formulario.addEventListener("submit", (e) => {
 
   let inputs = e.target.children;
 
-  let nombre=inputs[1].value;
-  let apellido=inputs[3].value;
-  let email= inputs[6].value;
-  let numeroTelefono= inputs[8].value;
+  let nombre = inputs[1].value;
+  let apellido = inputs[3].value;
+  let email = inputs[6].value;
+  let numeroTelefono = inputs[8].value;
   let tattooDescription = inputs[10].value;
   let placement = inputs[14].value;
-  let approximateSize= inputs[16].value;
-  let dates= inputs[19].value;
+  let approximateSize = inputs[16].value;
+  let dates = inputs[19].value;
 
-  let infoSolicitudes = localStorage.getItem("appointmentsRequests");
-  let solicitudes = JSON.parse(infoSolicitudes);
+  let fechaActual = new Date();
+  let fechaForm = new Date(dates);
+  
+  //Operador avanzado y libreria
+  fechaForm < fechaActual ? (Swal.fire({
+    icon: 'error',
+    title: 'Oops...',
+    text: 'Error. The date is earlier than the current one',
+    footer: ''
+  }) ) : (() => {
 
-  solicitudes.push(new Requests(nombre, apellido, email, numeroTelefono, tattooDescription, placement, approximateSize,dates ));
+    Swal.fire(
+      'Done!',
+      'Successfully sent!',
+      'success'
+    );
 
-  localStorage.setItem("appointmentsRequests", JSON.stringify(solicitudes));
+    let infoSolicitudes = localStorage.getItem("appointmentsRequests");
+    let solicitudes = JSON.parse(infoSolicitudes);
 
+    solicitudes.push(new Requests(nombre, apellido, email, numeroTelefono, tattooDescription, placement, approximateSize, dates));
+
+    localStorage.setItem("appointmentsRequests", JSON.stringify(solicitudes));
+  })()
 });
+
+
+
+
+
+
--- js/owner.js
+++ js/owner.js
@@ -1,15 +1,17 @@
 
-const botonn = document.getElementById("boton");
+const boton = document.getElementById("boton");
+const boton2 = document.getElementById("boton2");
 
 let contenedor = document.getElementById("contenedor");
 
 const renderizado = (solicitudes) => {
-    contenedor.innerHTML = "";
-  
-    solicitudes.forEach((item) => {
-      let div = document.createElement("div");
-      div.innerHTML = `
-        <h2>Request ${solicitudes.indexOf(item)+1}</h2>
+  contenedor.innerHTML = "";
+
+  solicitudes.forEach((item) => {
+    let div = document.createElement("div");
+    div.classList.add("req");
+    div.innerHTML = `
+        <h2>Request ${solicitudes.indexOf(item) + 1}</h2>
         <p>Name: ${item.nombre}</p>
         <p>Last name: ${item.apellido}</p>
         <p>Email: ${item.email}</p>
@@ -19,13 +21,40 @@ const renderizado = (solicitudes) => {
         <p>Approximate size: ${item.approximateSize}</p>
         <p>Dates available: ${item.dates}</p>
       `;
-  
-       contenedor.append(div);
-    });
+
+    contenedor.append(div);
+  });
 };
 
-  boton.onclick = () => {
-    let infoSolicitudes = localStorage.getItem("appointmentsRequests");
-    let solicitudes = JSON.parse(infoSolicitudes);
-    renderizado(solicitudes);
-  };
+boton.onclick = () => {
+  let infoSolicitudes = localStorage.getItem("appointmentsRequests");
+  let solicitudes = JSON.parse(infoSolicitudes);
+  renderizado(solicitudes);
+};
+
+boton2.onclick = () => {
+  contenedor.innerHTML = "";
+
+  fetch("../data.json")
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((item) => {
+        let div = document.createElement("div");
+        div.classList.add("req");
+        div.innerHTML = `
+                          <h2>Request ${data.indexOf(item) + 1}</h2>
+                          <p>Name: ${item.nombre}</p>
+                          <p>Last name: ${item.apellido}</p>
+                          <p>Email: ${item.email}</p>
+                          <p>Phone number: ${item.numeroTelefono}</p>
+                          <p>Tattoo description: ${item.tattooDescription}</p>
+                          <p>Placement: ${item.placement}</p>
+                          <p>Approximate size: ${item.approximateSize}</p>
+                          <p>Dates available: ${item.dates}</p>
+                        `;
+
+        contenedor.append(div);
+      });
+    });
+
+}
